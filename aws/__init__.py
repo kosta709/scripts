9fa51c520fdb16c343acad53bae95f222abdcaa8
Kosta Klevensky <kosta@codefresh.io>
2017-02-01 16:42:48 +0200

    added sg values for production

--- aws/__init__.py
+++ aws/__init__.py
@@ -7,14 +7,38 @@ C_TIMEOUT = 'TIMEOUT'
 def log(message):
     print(message)
 
+
 def add_sg(region, instance_filter, sg_ids, dryRun=False):
 
-# ii = ec2.describe_instances(Filters=[
-#         {
-#             'Name': 'tag:environment',
-#             'Values': [ 'aws-production']
-#         },
-#     ])
+    '''
+    region = 'us-east-1'
+    filter = [{'Name': 'tag:environment', 'Values': [ 'aws-staging']}]
+    sg_ids = ['sg-d35c65aa', 'sg-ef5ed293', 'sg-95fa54e9']
+
+    import aws
+    aws.add_sg(region, filter, sg_ids)
+
+
+    ### Production cf-prod - us-east-1 default vpc
+    region = 'us-east-1'
+    filter = [{'Name': 'tag:environment', 'Values': [ 'aws-production']}, {'Name': 'key-name', 'Values': ['cf-prod-1']}]
+    sg_ids = ['sg-8a26dbf0', 'sg-ef5ed293', 'sg-075bd77b']
+
+    ### Production cf-prod - us-east-1  vpc-customers-01
+    region = 'us-east-1'
+    filter = [{'Name': 'tag:environment', 'Values': ['aws-production']}, {'Name': 'key-name', 'Values': ['cf-customers-1']}]
+    sg_ids = ['sg-33d02949', 'sg-1859d564', 'sg-6450dc18']
+
+    ### Production cf-prod - eu-central-1 default vpc
+    region = 'eu-central-1'
+    filter = [{'Name': 'tag:environment', 'Values': [ 'aws-production', 'aws_production']}, {'Name': 'key-name', 'Values': ['cf-prod-1']}]
+    sg_ids = ['sg-8857d6e0', 'sg-9e3a22f6', 'sg-003c2468']
+
+    ### Production cf-prod - eu-central-1 customers vpc
+    region = 'eu-central-1'
+    filter = [{'Name': 'tag:environment', 'Values': [ 'aws-production', 'aws_production']}, {'Name': 'key-name', 'Values': ['cf-customers-1']}]
+    sg_ids = ['sg-a1a52dc9', 'sg-c33b23ab', 'sg-0731296f']
+    '''
 
     ec2 = boto3.client("ec2", region_name=region)
     ii = ec2.describe_instances(Filters=instance_filter)
@@ -43,19 +67,3 @@ def add_sg(region, instance_filter, sg_ids, dryRun=False):
             log("   Updated Success\n")
 
 
-#### For Staging
-'''
-region = 'us-east-1'
-filter = [{'Name': 'tag:environment', 'Values': [ 'aws-staging']}]
-sg_ids = ['sg-d35c65aa', 'sg-ef5ed293', 'sg-95fa54e9']
-
-import aws
-aws.add_sg(region, filter, sg_ids)
-'''
-
-### Production cf-prod - us-east-1 default vpc
-'''
-region = 'us-east-1'
-filter = [{'Name': 'tag:environment', 'Values': [ 'aws-production']}]
-sg_ids = ['sg-8a26dbf0', 'sg-ef5ed293', 'sg-075bd77b']
-'''
